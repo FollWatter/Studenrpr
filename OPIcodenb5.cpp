9360b15b1e6f894f44683734cf65e9729ae88b0b
FollWatter <FollWatter@ukr.net>
2020-10-28 17:54:13 +0200

    OPIcodenb5

---
+++ OPIcodenb5.cpp
@@ -0,0 +1,21 @@
+﻿
+#include "stdio.h"
+#include "windows.h"
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double S, Pi = 3.14, V, r, h = 4;
+    printf("Введіть радіус:");
+    scanf_s("%lf", &r);
+    S = 2 * Pi * r * h;
+    V = Pi * r * r * h;
+    printf("Площа бічної поверхні куба =%lf\n", S);
+    printf("Об'єм куба =%lf\n", V);
+    system("pause");
+    return 0;
+
+
+}
